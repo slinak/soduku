f7b85f9e8870405e283b0fe83c38793e0201d744
slinak <scott@linak.org>
2023-06-30 07:15:14 -0700

    formatting and variable renaming

--- Program.cs
+++ Program.cs
@@ -50,9 +50,13 @@ namespace InterviewApplication
         {
             int[][] board = new int[9][];
 
+            Console.WriteLine("Input rows as a single string, do not separate values.");
+            Console.WriteLine("Unknown values should be entered as 0");
+            Console.WriteLine("Press Enter after entering each row");
+            Console.WriteLine("Ex [ 1, 9, 8, 3, 0, 2, 5, 6, 7 ] is input as 198302567");
             for (int i = 0; i < 9; i++)
             {
-                Console.WriteLine(String.Format("Please input values for row {0} and press Enter. Use 0 for unknowns.", i));
+                Console.WriteLine(String.Format("Row {0}:", i));
                 var tmp = Console.ReadLine();
                 board[i] = ProcessBoardLineInput(tmp);
             }
@@ -69,15 +73,20 @@ namespace InterviewApplication
 
         public static void PrintBoard(int[][] board)
         {
-            foreach (var line in board)
-                Console.WriteLine(String.Format("{0} {1} {2} {3} {4} {5} {6} {7} {8}", line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8]));
+            for (int i = 0; i < 9; i++)
+            {
+                int[] line = board[i];
+                Console.WriteLine(String.Format("{0} {1} {2} | {3} {4} {5} | {6} {7} {8}", line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8]));
+                if (i == 2 || i == 5) 
+                    Console.WriteLine("---------------------");
+            }
         }
 
         public static void SolveBoard(int[][] board)
         {
-            int rows = 0;
-            int columns = 0;
-            int[] possibilities = new int[]{};
+            int rows, columns;
+            rows = columns = 0;
+            int[] possibleValues = new int[]{};
 
             if (IsFull(board))               
                 PrintBoard(board);
@@ -89,33 +98,33 @@ namespace InterviewApplication
                             rows = x;
                             columns = y;
                         }
-            
-            possibilities = GetPossibleEntries(board, rows, columns);
+
+            possibleValues = GetPossibleValues(board, rows, columns);
 
             for (int x = 1; x < 10; x++)
-                if(possibilities[x] != 0)
+                if(possibleValues[x] != 0)
                 {
-                    board[rows][columns] = possibilities[x];
+                    board[rows][columns] = possibleValues[x];
                     SolveBoard(board);
                 }
             
             board[rows][columns] = 0;
         }
 
-        public static int[] GetPossibleEntries(int[][] board, int i, int j)
+        public static int[] GetPossibleValues(int[][] board, int i, int j)
         {
-            int[] possibilities = new int[10];
+            int[] possibleValues = new int[10];
             
             for(int x = 1; x < 10; x++)
-                possibilities[x] = 0;
+                possibleValues[x] = 0;
 
             for(int y = 0; y < 9; y++)
                 if (board[i][y] != 0)
-                    possibilities[board[i][y]] = 1;
+                    possibleValues[board[i][y]] = 1;
 
             for (int a = 0; a < 9; a++)
                 if (board[a][j] != 0)
-                    possibilities[board[a][j]] = 1;
+                    possibleValues[board[a][j]] = 1;
 
             int k = 0;
             int l = 0;
@@ -137,25 +146,25 @@ namespace InterviewApplication
             for (int x = k; x < k + 3; x++)
                 for (int y = l; y < l + 3; y++)
                     if (board[x][y] != 0 )
-                        possibilities[board[x][y]] = 1;
+                        possibleValues[board[x][y]] = 1;
 
             for (int x = 1; x < 10; x++)
-                if (possibilities[x] == 0)
-                    possibilities[x] = x;
+                if (possibleValues[x] == 0)
+                    possibleValues[x] = x;
                 else
-                    possibilities[x] = 0;
+                    possibleValues[x] = 0;
             
-            return possibilities;
+            return possibleValues;
         }
 
 
         public static bool IsFull(int[][] board)
         {
-            for(int i = 0; i < 9; i++)
-                for(int j = 0; j < 9; j++)
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++)
                     if(board[i][j] == 0)
                         return false;
-            
+
             return true;
         }
     }
